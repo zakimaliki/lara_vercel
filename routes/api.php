7d0e3c5e18471b04a6ff47fc9abca07e572b28cd
malikizaki94 <malikizakitop@gmail.com>
2023-10-11 00:33:12 +0700

    back

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,11 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::prefix('test')->group(function () {
     Route::get('/user', [UserController::class, 'index']);
     Route::get('/user/{id}', [UserController::class, 'show']);
     Route::post('/user', [UserController::class, 'store']);
     Route::put('/user/{id}', [UserController::class, 'update']);
     Route::delete('/user/{id}', [UserController::class, 'destroy']);
-});
 
 
 
