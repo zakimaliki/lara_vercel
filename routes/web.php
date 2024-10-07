ae339d491a472abeea39dafc5bdaf5b50b9dc820
malikizaki94 <malikizakitop@gmail.com>
2024-10-07 14:02:14 +0700

    update terbaru

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,9 @@ Route::get('/user/{id}', [UserController::class, 'show']);
 Route::post('/user', [UserController::class, 'store']);
 Route::put('/user/{id}', [UserController::class, 'update']);
 Route::delete('/user/{id}', [UserController::class, 'destroy']);
+
+Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
+Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
+Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+Route::put('/movies/{id}', [MovieController::class, 'update'])->name('movies.update');
+Route::delete('/movies/{id}', [MovieController::class, 'destroy'])->name('movies.destroy');
