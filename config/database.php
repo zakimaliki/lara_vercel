a542679068245d8898f06d948a931ff40bc4d0b7
malikizaki94 <malikizakitop@gmail.com>
2024-10-07 12:40:37 +0700

    update db

--- config/database.php
+++ config/database.php
@@ -65,7 +65,6 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -73,10 +72,11 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'schema' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'), // Ubah ini menjadi prefer atau disable jika tidak menggunakan SSL
         ],
+
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
